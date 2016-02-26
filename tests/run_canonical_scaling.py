81292f211462a55046af9a877e5ffe959577cb8c
Fionn Malone <f.malone13@imperial.ac.uk>
2016-02-26 19:15:17 +0000

    Script to run and analyse canonical calc.

---
+++ tests/run_canonical_scaling.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '../uegpy')))
+import ueg_sys as ue
+import finite as fp
+import utils as ut
+import monte_carlo as mc
+
+time = float(sys.argv[1])
+beta = float(sys.argv[2])
+ne = float(sys.argv[3])
+
+cutoff = ut.kinetic_cutoff(ne, 1.0/beta)
+
+system = ue.System([0.1, ne, cutoff, 2])
+t_per_it = mc.sample_canonical_energy(system, beta/system.ef, 10)[1] / 10
+
+iterations = min(int(time/t_per_it), 1e6)
+
+(frame, time) = mc.sample_canonical_energy(system, beta/system.ef, iterations)
+
+print ("# Running uegpy version: %s"%(ut.get_git_revision_hash()))
+
+print ("# Time taken: %s s"%time)
+
+print frame.to_string(index=False)
+
