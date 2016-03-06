6bab6bbcf0e58580c2e94beada1f6478a731d1a0
Fionn Malone <f.malone13@imperial.ac.uk>
2016-03-06 15:10:26 +0000

    Script to do quick monte carlo calculation.

---
+++ tests/quick_mc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '../uegpy')))
+import ueg_sys as ue
+import finite as fp
+import infinite as inf
+import numpy as np
+import pandas as pd
+import monte_carlo as mc
+import utils as ut
+
+rs = float(sys.argv[1])
+ne = float(sys.argv[2])
+
+bvals = np.logspace(-1,1.5, 30)
+
+frame = []
+t = 0
+
+for b in bvals:
+    c = max(20, ut.kinetic_cutoff(ne, 1.0/b))
+    system = ue.System(rs, ne, c, 1)
+    (f, time) = mc.sample_canonical_energy(system, b/system.ef, 100)
+    t += time
+    frame.append(f)
+
+frame = pd.concat(frame)
+print ("# Running uegpy version: %s"%(ut.get_git_revision_hash()))
+print ("# Time taken: %s s"%time)
+print (frame.to_string(index=False))
