821234fb3cdfd2e744857aa577e7f4bcdca60a24
Fionn Malone <f.malone13@imperial.ac.uk>
2016-05-21 16:31:41 +0100

    Change default theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,7 +124,11 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+if os.environ.get('READTHEDOCS', None) == 'True':
+        html_theme = 'default'
+    else:
+        html_theme = 'haiku'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
