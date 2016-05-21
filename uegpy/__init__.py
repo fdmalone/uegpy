60252161b176db15c1c8246657a2851d4be6a6ee
Fionn Malone <f.malone13@imperial.ac.uk>
2016-05-21 17:17:32 +0100

    Add __init__.py most for documentation purposes.

---
+++ uegpy/__init__.py
@@ -0,0 +1,14 @@
+'''uegpy can calculate various thermodynamic properties of the UEG.
+
+The code is organised in various modules which can be combined to calculate
+useful quantities.  Currently the following functionality is available:
+
+* Internal, potential and free energies for the the 3D-UEG as a function of
+temperature at various levels of approximation. Currently we can do ideal,
+Hartree--Fock and RPA.
+* Dynamic properties such as density-density reposonse at any temperature.
+* Static and dynamic structure factors.
+
+Most of these quantities can be calculated in the thermodynamic limit as well as
+for a finite number of electrons in the canonical and grand canonical ensemble.
+'''
