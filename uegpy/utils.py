cbcc74290c6db61d16976ae2268404fc05ca4050
Fionn Malone <f.malone13@imperial.ac.uk>
2016-07-15 14:58:20 +0100

    Update where to look for revisions.

--- uegpy/utils.py
+++ uegpy/utils.py
@@ -273,10 +273,8 @@ sha1 : string
 
     src = sys.path[0]
 
-    sha1 = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
-                                   cwd=src).strip()
-    suffix = subprocess.check_output(['git', 'status', '--porcelain'],
-                                     cwd=src).strip()
+    sha1 = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+    suffix = subprocess.check_output(['git', 'status', '--porcelain', '../../uegpy']).strip()
     if suffix:
         return sha1 + '-dirty'
     else:
