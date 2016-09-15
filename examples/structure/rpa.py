4273d025cebe1fcfe945157aa56f7aa5c3c254a8
Fionn Malone <f.malone13@imperial.ac.uk>
2016-09-15 10:35:38 +0100

    Print out data frame.

--- examples/structure/rpa.py
+++ examples/structure/rpa.py
@@ -10,19 +10,24 @@ import structure as st
 import numpy as np
 import utils as ut
 import infinite as inf
+import pandas as pd
 
-qvals = np.linspace(0.1, 4, 10)
+qvals = np.linspace(0.1, 4.01, 40)
 
 zeta = 0
-rs = 0.01
-theta = 1.0
+rs = 1
+theta = 0.0625
 
 ef = ut.ef(rs, zeta)
 kf = (2.0*ef)**0.5
 beta = 1.0 / (theta*ef)
 mu = inf.chem_pot(rs, beta, ef, zeta)
 eta = beta * mu
-sq = [st.rpa_matsubara(q, rs, theta, eta, zeta, 1000) for q in qvals]
+sq = [st.rpa_matsubara(q, rs, theta, eta, zeta, 10000) for q in qvals]
+
+frame = pd.DataFrame({'q': qvals, 'S_q': sq}, columns=['q', 'S_q'])
+
+print (frame.to_string(index=False, justify='left'))
 
 pl.plot(qvals, np.array(sq), label=r'$\Theta=%s$'%theta)
 
