20e271d3e1a49b9ce904c208300479ed6413c5d3
Fionn Malone <f.malone13@imperial.ac.uk>
2016-09-25 21:00:43 +0100

    Update docstrings.

--- uegpy/size_corrections.py
+++ uegpy/size_corrections.py
@@ -8,7 +8,7 @@ import ueg_sys as ue
 
 
 def conv_fac(nmax, alpha):
-    ''' Convergence factor for Hartree--Fock size corrections from Drummond et al.
+    '''Convergence factor for Hartree--Fock size corrections from Drummond et al.
     Warning: Exceptionally slow.
 
 Parameters
@@ -112,13 +112,19 @@ theta : float
     Degeneracy temperature.
 zeta : int
     Spin polarisation.
-N : int
-    Number of electrons.
+kf : float
+    Fermi wave vector.
+kmin: float
+    Lower limit.
+kmax: float
+    Upper limit.
+lmax : int
+    Upper limit on Matsubara sum.
 
 Returns
 -------
-delta_V : float
-    RPA finite size correction.
+intg : float
+    Integral over s_q.
 '''
 
     def integrand(q, rs, theta, eta, zeta, kf, lmax):
@@ -134,9 +140,10 @@ delta_V : float
 
 
 def v_integral(rs, theta, eta, zeta, kf, kmin, kmax, lmax):
-    '''Potential energy correction using the BCDC finite size correction for V.
+    '''Potential energy evaluated from RPA structure factor.
 
-    Ref: Ethat W. Brown et al. Phys. Rev. Lett. 110, 146405 (2013).
+    Useful to evaluate size corrections.
+
 
 Parameters
 ----------
@@ -146,13 +153,19 @@ theta : float
     Degeneracy temperature.
 zeta : int
     Spin polarisation.
-N : int
-    Number of electrons.
+kf : float
+    Fermi wave vector.
+kmin: float
+    Lower limit.
+kmax: float
+    Upper limit.
+lmax : int
+    Upper limit on Matsubara sum.
 
 Returns
 -------
-delta_V : float
-    RPA finite size correction.
+V : float
+    RPA potential energy.
 '''
 
     def integrand(q, rs, theta, eta, zeta, kf, lmax):
@@ -168,9 +181,7 @@ delta_V : float
 
 
 def mad_integral(theta, eta, zeta, kf, kmin, kmax, nmax):
-    '''Potential energy correction using the BCDC finite size correction for V.
-
-    Ref: Ethat W. Brown et al. Phys. Rev. Lett. 110, 146405 (2013).
+    '''Evaluate integral of uniform distribution of point charges.
 
 Parameters
 ----------
@@ -180,13 +191,19 @@ theta : float
     Degeneracy temperature.
 zeta : int
     Spin polarisation.
-N : int
-    Number of electrons.
+kf : float
+    Fermi wave vector.
+kmin: float
+    Lower limit.
+kmax: float
+    Upper limit.
+lmax : int
+    Upper limit on Matsubara sum.
 
 Returns
 -------
-delta_V : float
-    RPA finite size correction.
+v_s: float
+   Potential energy from static distribution of point charges.
 '''
     def integrand(q, theta, eta, zeta, kf, nmax):
 
@@ -201,9 +218,7 @@ delta_V : float
 
 
 def v_summation(rs, theta, eta, zeta, kf, lmax, qvals, L):
-    '''Potential energy correction using the BCDC finite size correction for V.
-
-    Ref: Ethat W. Brown et al. Phys. Rev. Lett. 110, 146405 (2013).
+    '''Potential energy evaluated at discrete kpoints.
 
 Parameters
 ----------
@@ -213,13 +228,20 @@ theta : float
     Degeneracy temperature.
 zeta : int
     Spin polarisation.
-N : int
-    Number of electrons.
+kf : float
+    Fermi wave vector.
+lmax : int
+    Upper limit on Matsubara sum.
+qvals: list
+    magnitude of kpoints to evaluate structure factor at.
+L : float
+    Box length.
 
 Returns
 -------
-delta_V : float
-    RPA finite size correction.
+V: float
+    RPA potential energy evalutes in a finite box (assuming the structure factor
+            is converged).
 '''
     v = sum([ut.vq(q)*(st.rpa_matsubara(q/kf, rs, theta, eta, zeta, lmax)
               -1.0) for q in qvals])
@@ -227,39 +249,40 @@ delta_V : float
     return 1.0 / (2.0*L**3.0) * v
 
 
-def mad_summation(theta, eta, zeta, kf, nmax, qvals, L):
+def mad_summation(qvals, L):
+    '''Evaluate sum of uniform distribution of point charges.
 
+Parameters
+----------
+qvals: list
+    magnitude of kpoints to evaluate structure factor at.
+L : float
+    Box length.
+
+Returns
+-------
+v_s : float
+    Contribution to potential energy from periodically repeated point charges.
+'''
     v = sum([ut.vq(q)*(-1.0) for q in qvals])
 
     return 1.0 / (2.0*L**3.0) * v
 
 
-def fxc_correction_correct(rs, theta, zeta, ne, nmax, ecut):
-
-    return (
-        1.0 / (rs**2.0) * sc.integrate.quad(delta_v, 0, rs,
-                                        args=(theta, zeta, ne, nmax, ecut))[0]
-    )
-
-
 def fxc_correction_quad(rs, dv):
+    '''Evalute correction to exchange-correlation free energy.
+
+Parameters
+----------
+rs : list
+    Grid of densities.
+dv : list
+    Corresponding grid of potential energy size corrections.
+
+Returns
+-------
+df_xc : float
+    Size correction to f_xc.
+'''
 
     return rs[-1]**(-2.0) * sc.integrate.simps(rs*dv, rs)
-
-
-def delta_v(rs, theta, zeta, ne, nmax, ecut):
-
-    ef = ut.ef(rs, zeta)
-    kf = (2.0*ef)**0.5
-    beta = 1.0 / (ef*theta)
-    mu = inf.chem_pot(rs, beta, ef, zeta)
-
-    system = ue.System(rs, ne, ecut, zeta)
-    qvals = [system.kfac*(np.dot(k, k))**0.5 for k in system.kval[1:]]
-
-    v_int = v_integral(rs, theta, beta*mu, zeta, kf, 0, qvals[-1], nmax)
-    v_sum = v_summation(rs, theta, beta*mu, system.zeta, system.kf, nmax,
-                            qvals, system.L)
-    mad = ut.madelung_approx(system)
-
-    return v_int - (v_sum + 0.5*mad)
