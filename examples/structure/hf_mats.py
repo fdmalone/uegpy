b84a4268e599b87f23b5dc6cc9f352412daf3a63
Fionn Malone <f.malone13@imperial.ac.uk>
2016-09-25 11:54:18 +0100

    Updates to HF s_q comparison.

--- examples/structure/hf_mats.py
+++ examples/structure/hf_mats.py
@@ -20,18 +20,19 @@ kf = (2.0*ef)**0.5
 
 for t in tvals:
 
-    ef = ut.ef(rs, 1)
+    ef = ut.ef(rs, zeta)
     kf = (2.0*ef)**0.5
     beta = 1.0 / (t*ef)
-    mu = inf.chem_pot(rs, beta, ef, 1)
-    sq = [st.hartree_fock(q*kf, rs, beta, mu, 1) for q in qvals]
-    ef = ut.ef(rs, 1)
+    mu = inf.chem_pot(rs, beta, ef, zeta)
+    sq = [st.hartree_fock(q*kf, rs, beta, mu, zeta) for q in qvals]
+    ef = ut.ef(rs, zeta)
     kf = (2.0*ef)**0.5
     beta = 1.0 / (t*ef)
-    mu = inf.chem_pot(rs, beta, ef, 1)
-    sq2 = [st.hartree_fock_matsubara(q, t, rs, 1) for q in qvals]
+    mu = inf.chem_pot(rs, beta, ef, zeta)
+    sq2 = [st.hartree_fock_matsubara(q, rs, t, beta*mu, 0, 100) for q in qvals]
 
-    pl.plot(qvals, np.array(sq2)-np.array(sq), label=r'Alt: $\Theta=%s$'%t, linestyle='--', marker='o')
+    pl.plot(qvals, np.array(sq2), label=r'Alt: $\Theta=%s$'%t, marker='o')
+    pl.plot(qvals, np.array(sq), label=r'Alt: $\Theta=%s$'%t, marker='D')
 
 zeta = 0
 t = 1.0
@@ -39,7 +40,6 @@ ef = ut.ef(rs, zeta)
 kf = (2.0*ef)**0.5
 beta = 1.0 / (t*ef)
 mu = inf.chem_pot(rs, beta, ef, zeta)
-print (inf.hartree_fock_V(rs, t, zeta), inf.hfx_integral(rs, 1.0/ef, mu, zeta))
 
 pl.legend(loc='best')
 pl.show()
