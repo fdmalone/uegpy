4aa1743f8f9cb21c6e6dc72089f38e8df32a3118
Fionn Malone <f.malone13@imperial.ac.uk>
2016-10-01 15:11:56 +0100

    Particle convergence for dielectric function.

--- examples/finite/chi0.py
+++ examples/finite/chi0.py
@@ -9,6 +9,7 @@ import finite as fp
 import numpy as np
 import pandas as pd
 import dielectric as di
+import infinite as inf
 import matplotlib.pyplot as pl
 
 
@@ -18,26 +19,25 @@ nel = int(sys.argv[3])
 ecut = float(sys.argv[4])
 zeta = int(sys.argv[5])
 
-system = ue.System(rs, nel, ecut, zeta)
-beta = 1.0 / (system.ef * theta)
-mu = fp.chem_pot_sum(system, system.deg_e, beta)
+nvals = [38, 54, 204]
 
-omegas = [0.25, 1.0, 2.0, 4.0]
-qvals = [system.kfac/system.kf * np.dot(k, k)**0.5 for k in system.kval[1:]]
-
-for o in omegas:
-    chi0 = [di.lindhard_cplx_finite(o*system.ef, iq, system, beta, mu, 0.01) for (iq, q)
+for n in nvals:
+    system = ue.System(rs, n, ecut, zeta)
+    o = system.ef
+    beta = 1.0 / (system.ef * theta)
+    mu = fp.chem_pot_sum(system, system.deg_e, beta)
+    qvals = [system.kfac * np.dot(k, k)**0.5 for k in system.kval[1:]]
+    #print system.kfac
+    chi0 = [di.lindhard_cplx_finite(o*system.ef, iq, system, beta, mu, 0.662) for (iq, q)
             in enumerate(qvals)]
-    # chi0 = [di.im_lind_smeared(o*system.ef, q, beta, mu, 0.01) for (iq, q) in enumerate(qvals)]
-    chi03 = [di.re_lind(o*system.ef, q, beta, mu) for (iq, q) in enumerate(qvals)]
-    chi02 = [di.lindhard_cplx(o*system.ef, q, beta, mu, 0.01).real for (iq, q) in enumerate(qvals)]
-    pl.plot(qvals, [c.real for c in chi0], 'ro', label=r'Real: $\omega=%s$'%o,
-            markerfacecolor='None')
-    # pl.plot(qvals, [c.imag for c in chi0], 'bD', label=r'Imag: $\omega=%s$'%o)
-    # pl.plot(qvals, chi02, 'mv', markersize=5, label='complex TDL')
-    pl.plot(qvals, np.array(chi02), 'rD', markerfacecolor='None', label='REAL TDL')
-    pl.plot(qvals, np.array(chi03), 'rs', markerfacecolor='None', label='REAL TDL - 2')
-    pl.legend(numpoints=1)
-    pl.show()
-    pl.cla()
+    pl.plot(np.array(qvals)/system.kf, [c.imag/2.0 for c in chi0], label=r'Real: $N=%s$'%n, linewidth=1, marker='o')
 
+# chi0 = [di.im_lind_smeared(o*system.ef, q, beta, mu, 0.01) for (iq, q) in enumerate(qvals)]
+print system.kfac
+qvals = np.linspace(0, 8*system.kf, 100)
+mu = inf.chem_pot(rs, beta, system.ef, zeta)
+chi02 = [di.lindhard_cplx(o*system.ef, q, beta, mu, 0.01).imag for (iq, q) in enumerate(qvals)]
+pl.plot(np.array(qvals)/system.kf, np.array(chi02), '--', markerfacecolor='None', label='REAL TDL')
+pl.legend(numpoints=1)
+pl.show()
+pl.cla()
