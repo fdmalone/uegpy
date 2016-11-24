2d2740f2baa76d1261fefab6b6462ad5906a4b62
Fionn Malone <f.malone13@imperial.ac.uk>
2016-11-24 17:04:33 +0000

    Work on CPP version of self energy.

--- uegpy/cpp/dielectric.cpp
+++ uegpy/cpp/dielectric.cpp
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-#include <gsl/gsl_integration.h>
-#include <gsl/gsl_errno.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 static double const PI = 3.141592653589793;
 
-struct Lindhard {
-    double beta, mu, q, omega;
-};
+double trapezoid(vector<double> &f, double dx) {
 
-struct Angular {
-    double k, q, xi, beta, mu;
-};
+    double res;
 
-struct QInt {
-    double k, xi, beta, mu;
-};
+    res = f[0] + f[f.size()-1];
+
+    for (int i=1; i < f.size()-1; i++) {
+        res += 2.0*f[i];
+    }
+
+    return 0.5 * dx * res;
+
+}
 
 double fermi_factor(double ek, double mu, double beta) {
 
@@ -28,49 +29,46 @@ double fermi_factor(double ek, double mu, double beta) {
 
 double bose_factor(double ek, double beta) {
 
-    return 1.0 / (exp(beta*ek)-1);
+    double nb;
 
+    nb = 1.0 / (exp(beta*ek)-1);
+
+    if (nb > 1e5) {
+        return 1e5;
+    }
+    else {
+        return nb;
+    }
 }
 
-double re_lind_integrand(double k, void *p) {
+double re_lind_integrand(double k, double omega, double q, double beta, double mu) {
 
-    double k_pl, k_mi, omega, q, beta, mu;
-    Lindhard &params = *reinterpret_cast<Lindhard *>(p);
-
-    omega = params.omega;
-    q = params.q;
-    beta = params.beta;
-    mu = params.mu;
+    double k_pl, k_mi;
 
     k_pl = (0.5*q*q + omega) / q;
     k_mi = (0.5*q*q - omega) / q;
-
-    //std::cout << k_pl << "   " << k_mi << "   " << q << std::endl;
 
     return (k * fermi_factor(0.5*k*k, mu, beta) * (log(fabs((k_pl+k)/(k_pl-k))) + log(fabs((k_mi+k)/(k_mi-k)))));
 
 }
 
-double re_lind(double omega, double q, double beta, double mu) {
+double re_lind(double omega, double q, double beta, double mu, double kmax=10, double dk=0.01) {
 
-    double result, error;
-    Lindhard params;
-    gsl_integration_workspace * w = gsl_integration_workspace_alloc(1000);
-    gsl_function F;
+    double k, integral;
+    int its;
+    vector<double> I;
 
-    params.omega = omega;
-    params.q = q;
-    params.beta = beta;
-    params.mu = mu;
+    k = 0.001;
+    its = (int)(kmax/dk) + 1;
 
-    F.function = &re_lind_integrand;
-    F.params = reinterpret_cast<void *>(&params);
+    for (int i = 0; i < its; i++) {
+        I.push_back(re_lind_integrand(k, omega, q, beta, mu));
+        k = k + dk;
+    }
 
-    gsl_integration_qags(&F, 0, 200, 0, 1e-6, 1000, w, &result, &error);
+    integral = trapezoid(I, dk);
 
-    gsl_integration_workspace_free(w);
-
-    return (-1.0/(4*PI*PI*q*q) * result);
+    return (-1.0/(4*PI*PI*q) * integral);
 
 }
 
@@ -86,119 +84,73 @@ double im_lind(double omega, double q, double beta, double mu) {
 
 }
 
-double im_chi_rpa(double omega, double q, double beta, double mu) {
+double im_eps_inv(double omega, double q, double beta, double mu, double kmax=10, double eta=0.001) {
 
-    double vq, num, denom, re, im;
+    double vq;
+    double denom, re_eps, im_eps;
 
     vq = 4.0*PI / (q*q);
-    im = im_lind(omega, q, beta, mu);
-    re = 1.0 - vq*re_lind(omega, q, beta, mu);
-    denom = re*re + (vq*im)*(vq*im);
+    re_eps = 1.0 - vq * re_lind(omega, q, beta, mu, kmax);
+    im_eps = -vq * im_lind(omega, q, beta, mu);
+    denom = re_eps*re_eps + im_eps*im_eps + eta*eta;
 
-    return (im / denom);
+    return -im_eps / denom;
+}
+
+double angular_integral(double k, double omega, double q, double beta, double mu) {
+
+    vector<double> I;
+    double du = 0.01, u, ekq;
+    int imax;
+
+    imax = (int)(2/du) + 1;
+
+    u = -1.0;
+    for (int i = 0; i < imax; i++) {
+        ekq = 0.5*k*k + 0.5*q*q + k*q*u - mu;
+        I.push_back(im_eps_inv(ekq-omega, q, beta, mu) * (bose_factor(ekq-omega, beta)+fermi_factor(ekq, mu, beta)));
+        u = u + du;
+    }
+
+    return trapezoid(I, du);
 
 }
 
-double angular_integrand(double u, void *args) {
-
-    double omega, k, q, beta, mu, xi;
-    Angular &params =  *reinterpret_cast<Angular *>(args);
-
-    k = params.k;
-    q = params.q;
-    xi = params.xi;
-    beta = params.beta;
-    mu = params.mu;
-
-    omega = 0.5*k*k + 0.5*q*q + k*q*u;
-
-    //std::cout << u << "   "<< omega << "   " << k << "   " << q << std::endl;
-    return (4*PI/q*q) * im_chi_rpa(omega-xi, q, beta, mu) * (bose_factor(omega-xi, beta)+fermi_factor(omega, mu, beta));
-
-}
-
-double q_integrand(double q, void *args) {
-
-    double omega, k, beta, mu, xi, result, error;
-    QInt &params = *reinterpret_cast<QInt *>(args);
-    Angular p2;
-
-    gsl_integration_workspace * w = gsl_integration_workspace_alloc(1000);
-    gsl_function F;
-
-    k = params.k;
-    xi = params.xi;
-    beta = params.beta;
-    mu = params.mu;
-
-    p2.k = k;
-    p2.q = q;
-    p2.xi = xi;
-    p2.beta = beta;
-    p2.mu = mu;
-
-    //std::cout << "qint: " << q << std::endl;
-
-    F.function = &angular_integrand;
-    F.params = reinterpret_cast<void *>(&p2);
-
-    gsl_integration_qags(&F, -1, 1, 0, 1e-3, 1000, w, &result, &error);
-    //std::cout << q << "    " << result << std::endl;
-
-    gsl_integration_workspace_free(w);
-
-    return result;
-
-}
-
-double im_sigma_rpa(double xi, double k, double beta, double mu) {
+double im_sigma_rpa(double omega, double q, double beta, double mu, double qmax=10) {
 
     double result, error;
-    QInt params;
-    gsl_integration_workspace * w = gsl_integration_workspace_alloc(1000);
-    gsl_function F;
+    double dq = 0.1, k;
+    vector<double> I;
 
-    params.k = k;
-    params.xi = xi;
-    params.beta = beta;
-    params.mu = mu;
+    int imax = (int)qmax/dq + 1;
 
-    F.function = &q_integrand;
-    F.params = reinterpret_cast<void *>(&params);
+    k = 0.001;
+    for (int i = 0; i < imax; i++) {
+        I.push_back(angular_integral(k, omega, q, beta, mu));
+        k += dq;
+    }
 
-    gsl_integration_qags(&F, 0.01, 100, 0, 1e-3, 1000, w, &result, &error);
-    //q_integrand(0.1, F.params);
-
-    //double dq = 0.1;
-    //for (int qv = 0; qv < 2000; qv++) {
-        ////std::cout << dq << std::endl;
-        //result = q_integrand(dq, F.params);
-        ////result = angular_integrand(dq, F.params);
-        //std::cout << dq << "   " << result << std::endl;
-        //dq += 0.001;
-    //}
-    gsl_integration_workspace_free(w);
-
-    return (1.0/PI) * result;
+    return (1.0/PI) * trapezoid(I, dq);
 
 }
 
 int main() {
 
-    double mu = -0.039521;
-    double beta = 0.5430107179652064;
-    double k, omega;
+    double mu = 1.84158;
+    double beta = 100;
+    double k, omega, kf, im, re, inv;
+    vector<double> I;
 
-    omega = 2.0;
-    gsl_set_error_handler_off();
-    for (int o = 0; o < 1; o++) {
-        omega -= 0.1;
+    omega = -2.0;
+    kf = sqrt((2*mu));
+    for (int o = 0; o < 400; o++) {
+        omega += 0.01;
         k = 0.2;
-        for (int kit = 0; kit < 1; kit++) {
-            //k += 0.5;
-            std::cout << k << "   " << omega << "   " << im_sigma_rpa(omega, k, beta, mu) << endl;
-        }
+        //im = im_lind(omega, k, beta, mu);
+        //re = re_lind(omega, k, beta, mu, kf);
+        im = im_sigma_rpa(omega, k, beta, mu);
+        std::cout << omega << "   " << im << endl;
     }
-    //std::cout << im_sigma_rpa(2, 1, beta, mu) << std::endl;
+    k = 0.0;
 
 }
