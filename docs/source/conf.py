056f9c99ab5d016a76f0ac06b046d758af6651b7
Fionn Malone <f.malone13@imperial.ac.uk>
2017-12-27 17:14:33 -0800

    Add mock module.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
-                'scipy.interpolate', 'scipy.constants']
+                'scipy.interpolate', 'scipy.constants', 'pandas']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
